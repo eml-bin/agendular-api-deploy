f58e80cb0a4d368046d75e84b5bf7b30030ef7b0
Eduardo ML <edu@eml.run>
2023-11-16 01:36:25 -0600

    GET dev02

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 
 * Visualización de rutas básicas en express.js
 
+### GET (dev02)
+
+* Rutas GET de REST
+* Params (obligatorios)
+* Query-Params (opcionales)
+* Choque de Rutas (issue)
--- index.js
+++ index.js
@@ -10,16 +10,61 @@ app.get('/', (req, res) => {
 
 })
 
-app.get('/items', (req, res) => {
-    // req. request
-    // res. response
+// GET
 
-    res.json({ 
-        nombre: "item 1",
-        cantidad: 3
-    })
+app.get('/items', (req, res) => {
+    // req. request (solicitud/petición)
+    // res. response (respuesta)
+
+    const { category } = req.query
+
+    const ITEMS = [
+        { 
+            nombre: "item 1",
+            cantidad: 3,
+            category: "A"
+        },
+        { 
+            nombre: "item 2",
+            cantidad: 1,
+            category: "B"
+        }
+    ]
+
+    if (category) {
+        // El método filter devuelve siempre un arreglo
+        res.json(ITEMS.filter(item => item.category === category))
+    } else {
+        res.json(ITEMS)
+    }
 
 })
+
+app.get('/items/view', (req, res) => {
+    res.type('html').send('<p>Items View</p>')
+})
+
+app.get('/items/:id', (req, res) => {
+    // req. request (solicitud/petición)
+    // res. response (respuesta)
+    
+    const { id } = req.params; // # params
+
+    res.json(
+        { 
+            id: id, 
+            nombre: "item 1",
+            cantidad: 3,
+            category: "A"
+        }
+    )
+
+})
+
+// Ocasiona un issue de routing (choque de rutas)
+// app.get('/items/view', (req, res) => {
+//     res.type('html').send('<p>Items View</p>')
+// })
 
 app.listen(PORT, () => {
     // eslint-disable-next-line no-console

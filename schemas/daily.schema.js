f2327eb26a64f17ce465d83bc890b4c65ecf41f8
Eduardo ML <edu@eml.run>
2023-11-18 02:03:00 -0600

    correcciones finales

--- schemas/daily.schema.js
+++ schemas/daily.schema.js
@@ -3,15 +3,18 @@
 const Joi = require('joi')
 
 const note = Joi.string().min(15).max(150)
+const emotion = Joi.string()
 
 // const price = Joi.number().integer().min(10)
 
 const createDailySchema = Joi.object({
     note: note.required(),
+    emotion: emotion.required()
 })
 
 const updateDailySchema = Joi.object({
     note: note,
+    emotion: emotion
 })
 
 module.exports = { 

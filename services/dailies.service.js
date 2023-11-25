9e70b072fe6f408b4d337aec957bc2092d3373db
Eduardo ML <edu@eml.run>
2023-11-25 08:13:42 -0600

    save progress

--- services/dailies.service.js
+++ services/dailies.service.js
@@ -48,7 +48,11 @@ class DailiesService {
     // Crear método para obtener todos (dev06)
     async all() {
         // Utilizar findAll de ORM Sequelize (dev07)
-        const dailies = await models.Daily.findAll()
+        const dailies = await models.Daily.findAll({
+            order: [
+                ['timestamp', 'DESC']
+            ]
+        })
         return dailies
 
 
